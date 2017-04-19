d72904e693637765079a188f9ea81de42ff0635f
David D. Riddle <ddriddle@illinois.edu>
2017-04-19 13:03:11 -0500

    Updated view!

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,5 +3,5 @@ import datetime
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    html = "<html><body>The current time is %s.</body></html>" % now
     return HttpResponse(html)
