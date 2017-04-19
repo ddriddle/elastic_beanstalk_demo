fb469aea99d136912d708f7eb697f6c9ee80b9d0
David D. Riddle <ddriddle@illinois.edu>
2017-04-19 13:19:52 -0500

    Fixed pep8 error

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 import datetime
 
+
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>The current time is %s.</body></html>" % now
