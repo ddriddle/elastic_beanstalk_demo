48a07b6240a856361df05c3b33500be73c08d73d
David D. Riddle <ddriddle@illinois.edu>
2017-06-08 08:26:46 -0500

    Back to hello world!

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,7 +3,7 @@ import datetime
 
 
 def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>My time is %s.</body></html>" % now
-    # html = "<html><body>Hello World!</body></html>"
+    # now = datetime.datetime.now()
+    # html = "<html><body>My time is %s.</body></html>" % now
+    html = "<html><body>Hello World!</body></html>"
     return HttpResponse(html)
